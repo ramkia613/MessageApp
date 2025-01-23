c100a20fe35a139ca09a3c9d81dccd7063135c77
Ramki Anbazhagan <ramki613@gmail.com>
2025-01-23 18:04:02 +0700

    Unit test added

---
+++ Message.Tests/MessageTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Message;
+
+namespace Message.Tests;
+
+public class MessageTests
+{
+    
+    [Fact]
+    public void TestEmptyInput()
+    {
+        string input = "";
+        string expected = "";
+        string result = Message.OldPhonePad(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void TestInput()
+    {
+        string input = "222 2 22#";
+        string expected = "CAB";
+        string result = Message.OldPhonePad(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void TestInputWithSpecialChar()
+    {
+        string input = "222 2 221222 2 777#";
+        string expected = "CAB&CAR";
+        string result = Message.OldPhonePad(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void TestInputWithBackSpace()
+    {
+        string input = "8 88777444666*664#";
+        string expected = "TURING";
+        string result = Message.OldPhonePad(input);
+
+        Assert.Equal(expected, result);
+    }
+}
